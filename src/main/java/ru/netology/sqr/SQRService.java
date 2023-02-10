66069b7bffa95496bf1a3c58feba2099c786b40a
Тетикова Наталья <ntetikva@gmail.com>
2023-02-10 15:51:54 +0300

    added homework

---
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -0,0 +1,16 @@
+package ru.netology.sqr;
+
+public class SQRService {
+    public int calcSqrt(int x, int y) {
+        int count = 0;
+        for (int i = 10; i <= 99; i++) {
+
+            if ((i * i >= x) && (i * i <= y)) {
+                count = count + 1;
+            }
+        }
+        return count;
+    }
+}
+
+
